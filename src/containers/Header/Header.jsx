1afe86f81ac9e8dd42d78f5e1a23babfb1342162
Aurélien St <aurelien.strimelle@bstorm.be>
2024-05-24 16:08:31 +0200

    Header 🎨

---
+++ src/containers/Header/Header.jsx
@@ -0,0 +1,13 @@
+import style from './Header.module.css';
+
+const Header = () => {
+
+    return (
+
+        <header className={style.header}>
+            <p>Demo 04 - Communication</p>
+        </header>
+    );
+};
+
+export default Header
